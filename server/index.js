e05e1f5ef44834c04627ff72341799ff9f1a9019
smrkarkii <076bct080.smriti@pcampus.edu.np>
2022-11-25 14:01:38 +0545

    payment feature on work

--- server/index.js
+++ server/index.js
@@ -5,11 +5,23 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 dotenv.config();
 const event_routes = require("./routes/event");
+const ejs = require("ejs");
+
+app.set("view engine", "ejs");
 //body-parser
 app.use(bodyParser.json());
 
 //routing
 app.use("/", event_routes);
+app.get("/home", (req, res) => {
+  res.render("pay.ejs");
+});
+app.get("/success", (req, res) => {
+  res.render("success.ejs");
+});
+app.get("/failure", (req, res) => {
+  res.render("failure.ejs");
+});
 //app.use("/", )
 
 //database connecting to mongo db
