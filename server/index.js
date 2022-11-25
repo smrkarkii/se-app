1ec0dcbf2efdeb00b51cd8bbc08c283ce905fba2
smrkarkii <076bct080.smriti@pcampus.edu.np>
2022-11-25 17:16:03 +0545

    admin templates first

--- server/index.js
+++ server/index.js
@@ -5,7 +5,17 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 dotenv.config();
 const event_routes = require("./routes/event");
+// const user_routes = require("./routes/user");
+// const service_routes = require("./routes/service");
+const admin_routes = require("./routes/admin.js");
 //body-parser
+
+mongoose.connect(process.env.MONGO_DB).then(() => {
+  console.log("mongo db connected");
+});
+
+app.use("/admin", admin_routes);
+
 app.use(bodyParser.json());
 
 //routing
@@ -13,9 +23,8 @@ app.use("/", event_routes);
 //app.use("/", )
 
 //database connecting to mongo db
-mongoose.connect(process.env.MONGO_DB).then(() => {
-  console.log("mongo db connected");
-});
+
 app.listen(8000, () => {
   console.log("Running");
+  console.log("admin Running at /admin");
 });
