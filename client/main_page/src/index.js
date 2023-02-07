4336368a780ddd6e62a924c7f9d773f51c4738ff
smrkarkii <076bct080.smriti@pcampus.edu.np>
2023-02-07 20:15:43 +0545

    some

--- client/main_page/src/index.js
+++ client/main_page/src/index.js
@@ -3,11 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Navbar from "./Home/pages_elements/Navbar";
-
 
 //Routes
 import Gallery from "./Pages/Gallery";
@@ -15,13 +13,11 @@ import Events from "./Pages/Events.js";
 import Reservations from "./Pages/Reservations.js";
 import Contact from "./Pages/contact";
 import Login from "./Pages/Login";
+import PageContent from "./components/PageContent";
 import Next from "./Pages/Next.js";
-
 
 ReactDOM.render(
   <React.StrictMode>
-
-    
     <Navbar />
     <Router>
       <Routes>
@@ -32,6 +28,7 @@ ReactDOM.render(
         <Route exact path="/contact" element={<Contact />} />
         <Route exact path="/login" element={<Login />} />
         <Route exact path="/next" element={<Next />} />
+        <Route exact path="/admin" element={<PageContent />} />
       </Routes>
     </Router>
   </React.StrictMode>,
