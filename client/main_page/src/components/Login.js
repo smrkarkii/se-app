ec8c300d4e039cc92c86ec14e6413597f13b5035
smrkarkii <076bct080.smriti@pcampus.edu.np>
2023-02-11 20:20:41 +0545

    front end and backedn resolved

--- client/main_page/src/components/Login.js
+++ client/main_page/src/components/Login.js
@@ -9,7 +9,7 @@ const Login = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch(`http://localhost:8000/api/auth/login`, {
+    const response = await fetch(`http://localhost:8000/users/login`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -130,7 +130,16 @@ const Login = () => {
                     name="password"
                   />
                 </div>
-                <button type="submit" className="btn btn-primary mt-2">
+                <button
+                  style={{
+                    backgroundColor: "#00028d",
+                    borderRadius: "10px",
+                    cursor: "pointer",
+                  }}
+                  type="submit"
+                  className="btn btn-primary mt-2"
+                >
+                  {" "}
                   Login
                 </button>
               </form>
