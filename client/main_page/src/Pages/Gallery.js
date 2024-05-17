9934cfad7ec41aec4a3c3f16167c965dc9bb5b43
smrkarkii <076bct080.smriti@pcampus.edu.np>
2024-05-17 22:59:38 +0545

    change in gallery photos

--- client/main_page/src/Pages/Gallery.js
+++ client/main_page/src/Pages/Gallery.js
@@ -4,7 +4,12 @@ import ictcs from "./Gallery/ictc.jpg";
 import labarrangement from "./Gallery/labarrangements.jpg";
 import complab from "./Gallery/complab.jpg";
 import switches from "./Gallery/switches.jpg";
-const images = [ictcs, labarrangement, complab, switches];
+const images = [
+  "https://www.wms.com.np/sites/default/files/photo5.jpg",
+  "https://bmc.ac.in/inner/assets/images/laboratory/computer.jpg",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1saVj2jY1E3CsY4_2IFs8L_qE_fuWapdkQ7Q2nq5uRg&s",
+  switches,
+];
 
 const Gallery = () => {
   return (
